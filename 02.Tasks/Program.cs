e6e11ddd74c4c410605730290cfb1ca8047ad094
allseen <allseen@live.ru>
2023-07-15 17:51:18 +0300

    02.Tasks

---
+++ 02.Tasks/Program.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace ArraySortTests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkSwitcher
+             .FromAssembly(typeof(Program).Assembly)
+             .Run(args, new BenchmarkDotNet.Configs.DebugInProcessConfig());
+
+
+            BenchmarkRunner.Run<SortTests>();
+        }
+    }
+}
